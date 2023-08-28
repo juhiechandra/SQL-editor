eaea22be6b35dd311033de3a25578620c4d97612
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-08-28 11:02:26 +0530

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,15 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+  extends: ["react-app"],
+  rules: {},
+  overrides: [
+    {
+      files: ["**/*.js?(x)"],
+      rules: {
+        // ******** add ignore rules here *********
+        "react/no-unescaped-entities": "off",
+        "react/display-name": "on",
+        "react/prop-types": "on",
+      },
+    },
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+};
