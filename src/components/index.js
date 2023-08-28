332c6296b4185e4e1b633c19fee17508cc271960
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-08-29 00:27:14 +0530

    refactor: exporting components from one file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as SearchHistory } from "./history/searchHistory";
+export { default as HomeNavbar } from "./navbar/homeNavbar";
+export { default as CodeEditor } from "./editor/codeEditor.jsx";
+export { default as SearchOutput } from "./output/searchOutput";
+export { default as AvailableData } from "./tables/searchTables";
+export { default as NoOutput } from "./placeholders/showNoOutput";
