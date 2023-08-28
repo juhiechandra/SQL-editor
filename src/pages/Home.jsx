2f49a7d10d652a670fbf46c8b43ad1cb1f7f3f1e
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-08-29 00:26:25 +0530

    feat: called navbar, editor, history & output
    refactored some functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  CodeEditor,
+  SearchOutput,
+  AvailableData,
+  HomeNavbar,
+  SearchHistory,
+} from "../components";
 import useDataFetcher from "/src/hooks/useDataFetcher.js";
 
 function Home() {
@@ -13,35 +20,91 @@ function Home() {
   const [hideTableSearchHistory, setHideTableSearchHistory] = useState(false);
   const { data, isLoading } = useDataFetcher(executedQuery);
 
-  function handleSubmit() {
-    SearchOutputTerminal = true;
-    executedQuery = query;
-    history = [...history, query];
+  const handleSubmit = useCallback(() => {
+    setSearchOutputTerminal(true);
+    setExecutedQuery(query);
+    setHistory([...history, query]);
     localStorage.setItem(
       "history",
       JSON.stringify({ items: [...history, query] })
     );
-  }
+  }, [query]);
 
-  outputData = data;
-  isOutputLoad = isLoading;
+  useEffect(() => {
+    setOutputData(data);
+    setOutputLoad(isLoading);
+  }, [data, isLoading]);
 
-  function handleFileImport(e) {
+  // file importer handler
+  const handleFileImport = useCallback((e) => {
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = (event) => {
         const content = event.target.result;
-        query = content;
+        setQuery(content);
       };
       reader.readAsText(file);
     }
-  }
+  }, []);
 
-  SearchOutputTerminal = false;
-  hideSearchHistory = true;
+  useEffect(() => {
+    setSearchOutputTerminal(false);
+    setHideSearchHistory(true);
+  }, []);
 
-  return <div></div>;
+  return (
+    <div className="block md:flex item-center justify-between h-[100vh] w-[100vw] overflow-hidden bg-[#2C3137] text-[white]">
+      <AvailableData
+        hideTableSearchHistory={hideTableSearchHistory}
+        setHideTableSearchHistory={setHideTableSearchHistory}
+        setQuery={setQuery}
+        fullScreen={fullScreen}
+      />
+
+      <div
+        className={`w-full border-x-[1px] border-[#00000033] ${
+          hideSearchHistory && hideTableSearchHistory
+            ? "flex-[0.95]"
+            : hideSearchHistory || hideTableSearchHistory
+            ? "flex-[0.8]"
+            : "flex-[0.60]"
+        } overflow-x-scroll h-[100vh] overflow-y-hidden`}
+      >
+        <HomeNavbar
+          handleSubmit={handleSubmit}
+          handleFileImport={handleFileImport}
+          query={query}
+          setQuery={setQuery}
+          setFullScreen={setFullScreen}
+          fullScreen={fullScreen}
+        />
+        <CodeEditor
+          setQuery={setQuery}
+          query={query}
+          SearchOutputTerminal={SearchOutputTerminal}
+          fullScreen={fullScreen}
+        />
+        <SearchOutput
+          hideTableSearchHistory={hideTableSearchHistory}
+          hideSearchHistory={hideSearchHistory}
+          isOutputLoad={isOutputLoad}
+          setOutputLoad={setOutputLoad}
+          outputData={outputData}
+          setOutputData={setOutputData}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+      <SearchHistory
+        history={history}
+        setHistory={setHistory}
+        hideSearchHistory={hideSearchHistory}
+        setHideSearchHistory={setHideSearchHistory}
+        setQuery={setQuery}
+        fullScreen={fullScreen}
+      />
+    </div>
+  );
 }
 
 export default Home;
